4808be8d126f3d9718e433e46819e138cad7b1ea
zynpksnoglu <zeynep.kesanoglu@ogr.gelisim.edu.tr>
2024-10-18 11:40:43 +0300

    Add files via upload

---
+++ hafta3.php
@@ -0,0 +1,55 @@
+<?php
+//isset,unset kullanım
+/*echo"isset($AdiSoyadi)";
+$AdiSoyadi="zeynep kesan";
+echo"isset($AdiSoyadi)";
+
+$değisken="Gelişim üniversitesi";
+echo $değisken."<br>";
+unset($degisken);
+echo $degisken; 
+
+//doğum yılı hesaplama
+
+$yil=2024;
+$yil1=2005;
+$yas=$yil-$yil1;
+echo "benim yaşım".$yas; 
+
+// basit hesap makinesi
+
+$sayi1=35;
+$sayi2=5;
+
+$topla=$sayi1+$sayi2;
+$cikar=$sayi1-$sayi2;
+$carp=$sayi1*$sayi2;
+$bol=$sayi1/$sayi2;
+$mod=$sayi1%$sayi2;
+$us=$sayi1**$sayi2;
+
+echo "1.sayının değeri: $sayi"."<br>";
+echo "2.sayının değeri: $say2"."<br>";
+echo "toplama işleminin sonucu;:$topla "."<br>";
+echo "çıkarma işleminin sonucu;:$cikar "."<br>";
+echo "çarpma işleminin sonucu;:$carp "."<br>";
+echo "bölme işleminin sonucu;:$bol "."<br>";
+echo "mod alma işleminin sonucu;:$mod "."<br>";
+echo "üs alma işleminin sonucu;:$us "."<br>";*/
+
+
+
+// sayı eksiltme arttırma
+
+$sayi=15;
+
+echo "<u>yazmış oldunuz sayının değeri:</u>".$sayi."<br>".
+"sayının degerini artıma işlemi:".++$sayi."<br>".
+"sayının degerini azaltma işlemi:".--$sayi;
+
+
+
+
+
+
+?>
